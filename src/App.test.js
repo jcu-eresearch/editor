d6326d96e501b2dee3d430b38dc9fc277b7e4bc6
Tim Englart <timenglartdev@gmail.com>
2021-07-19 11:12:41 +1000

    add smoke tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,44 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import App from './App'
+import userEvents from '@testing-library/user-event'
 
-test('renders learn react link', () => {
+test('renders the app', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+
+  // Header
+  expect(() => screen.getByText(/Research Data JCU – Visual Editor/i)).not.toThrow()
+
+  // Footer
+  expect(() => screen.getByText(/Research Data JCU team/i)).not.toThrow()
+
+  // Copy all button
+  expect(() => screen.getByText(/copy all/i)).not.toThrow()
+})
+
+test('copy all function works', async () => {
+  document.writeText = jest.fn();
+
+  const writeTextFn = jest.fn(async () => {})
+
+  Object.assign(navigator, {
+    clipboard: {
+      writeText: writeTextFn,
+    },
+  });
+
+  render(<App />)
+
+  // Copy all button
+  expect(() => screen.getByText(/copy all/i)).not.toThrow()
+
+  const copyAllButton = screen.getByText(/copy all/i)
+
+  // Copy all button
+  userEvents.click(copyAllButton)
+
+  // Text Copied to Clipboard
+  await waitFor(() =>{
+    expect(writeTextFn).toBeCalledTimes(1)
+    screen.getByText(/Content copied to clipboard!/i)
+  })
 })
